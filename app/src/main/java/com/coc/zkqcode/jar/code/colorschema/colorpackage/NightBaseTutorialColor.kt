1341437292248bd37efcf20b8af7caae0b1a8e8d
aitutor2025 <simonwanghkteacher@gmail.com>
2026-02-01 18:43:35 +0800

    refactor night tutorial color, and start to do troop training.

---
+++ app/src/main/java/com/coc/zkqcode/jar/code/colorschema/colorpackage/NightBaseTutorialColor.kt
@@ -0,0 +1,4 @@
+package com.coc.zkqcode.jar.code.colorschema.colorpackage
+
+class NightBaseTutorialColor {
+}
---
+++ app/src/main/java/com/coc/zkqcode/jar/code/mainbase/MainBaseScript.kt
@@ -0,0 +1,4 @@
+package com.coc.zkqcode.jar.code.mainbase
+
+class MainBaseScript {
+}
