5cc1a1cffb7739f7cd7a4e0e2736e486d2efb110
Bob8259 <azikaban_yuki@outlook.com>
2026-02-08 20:33:13 +0800

    add colors

--- app/src/main/java/com/coc/zkqcode/jar/code/MainScript.kt
+++ app/src/main/java/com/coc/zkqcode/jar/code/MainScript.kt
@@ -79,7 +79,12 @@ object MainScript {
     private suspend fun runTestCode() {
         while (true) {
             ShowMessage("测试代码开始")
-
+//            runApp("com.supercell.clashofclans2")
+//            enterMainScreen()
+            delayWithMultiplier(500)
+//            NightBaseUpgradeBuildings.buildAllNewBuildings()
+            val greenTick = nightBaseFindBuildButton(type = "Cross")
+            ShowMessage("x ${greenTick?.x}, y ${greenTick?.y}")
             ShowMessage("测试代码结束")
             delayWithMultiplier(500)
         }
