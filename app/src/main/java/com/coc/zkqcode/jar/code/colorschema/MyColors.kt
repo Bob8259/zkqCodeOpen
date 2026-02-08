796c78ba7b1784f75c3a46d3f566affb174f9cc4
Bob8259 <azikaban_yuki@outlook.com>
2026-02-08 14:58:14 +0800

    ready to make build wall

--- app/src/main/java/com/coc/zkqcode/jar/code/colorschema/MyColors.kt
+++ app/src/main/java/com/coc/zkqcode/jar/code/colorschema/MyColors.kt
@@ -1,24 +1,23 @@
 package com.coc.zkqcode.jar.code.colorschema
 
-import androidx.compose.ui.graphics.Color
 import com.coc.zkqcode.jar.code.colorschema.colorpackage.FeatureColors
 import com.coc.zkqcode.jar.code.colorschema.colorpackage.IFeatureColors
+import com.coc.zkqcode.jar.code.colorschema.colorpackage.IMainBaseTraining
 import com.coc.zkqcode.jar.code.colorschema.colorpackage.IMainBaseTutorial
+import com.coc.zkqcode.jar.code.colorschema.colorpackage.INightBaseObstaclesRemovalColors
+import com.coc.zkqcode.jar.code.colorschema.colorpackage.INightBaseResourcesColors
+import com.coc.zkqcode.jar.code.colorschema.colorpackage.INightBaseTrainingColors
 import com.coc.zkqcode.jar.code.colorschema.colorpackage.INightBaseTutorial
 import com.coc.zkqcode.jar.code.colorschema.colorpackage.IUIColors
 import com.coc.zkqcode.jar.code.colorschema.colorpackage.IWorkerColors
-import com.coc.zkqcode.jar.code.colorschema.colorpackage.IMainBaseTraining
 import com.coc.zkqcode.jar.code.colorschema.colorpackage.MainBaseTraining
 import com.coc.zkqcode.jar.code.colorschema.colorpackage.MainBaseTutorial
+import com.coc.zkqcode.jar.code.colorschema.colorpackage.NightBaseObstaclesRemovalColors
+import com.coc.zkqcode.jar.code.colorschema.colorpackage.NightBaseResourcesColors
+import com.coc.zkqcode.jar.code.colorschema.colorpackage.NightBaseTrainingColors
 import com.coc.zkqcode.jar.code.colorschema.colorpackage.NightBaseTutorial
 import com.coc.zkqcode.jar.code.colorschema.colorpackage.UIColors
 import com.coc.zkqcode.jar.code.colorschema.colorpackage.WorkerColors
-import com.coc.zkqcode.jar.code.colorschema.colorpackage.INightBaseResourcesColors
-import com.coc.zkqcode.jar.code.colorschema.colorpackage.NightBaseResourcesColors
-import com.coc.zkqcode.jar.code.colorschema.colorpackage.INightBaseObstaclesRemovalColors
-import com.coc.zkqcode.jar.code.colorschema.colorpackage.NightBaseObstaclesRemovalColors
-import com.coc.zkqcode.jar.code.colorschema.colorpackage.INightBaseTrainingColors
-import com.coc.zkqcode.jar.code.colorschema.colorpackage.NightBaseTrainingColors
 
 object MyColors : IWorkerColors by WorkerColors, IUIColors by UIColors, IFeatureColors by FeatureColors,
     IMainBaseTutorial by MainBaseTutorial, INightBaseTutorial by NightBaseTutorial,
@@ -36,5 +35,29 @@ object MyColors : IWorkerColors by WorkerColors, IUIColors by UIColors, IFeature
         0,
         0.9,
         "需要将大本营升至6级"
+    )
+
+    //The followings are Night Base Upgrade color
+    val InnerShopArrow: ColorSchema = ColorSchema.parse(
+        736,
+        313,
+        781,
+        363,
+        "00A3FD",
+        "9|0|00A2FD,18|0|01A0FF,27|0|08A7FF,36|0|13AEFF,0|25|02ABFF,9|25|01ABFE,18|25|01A6FD,27|25|04A1FF,36|25|0C9BFF",
+        0,
+        0.9,
+        "商店内部箭头"
+    )
+    val WallInShop: ColorSchema = ColorSchema.parse(
+        618,
+        401,
+        658,
+        453,
+        "EADEAC",
+        "8|0|567188,16|0|5C96CD,24|0|7BDBF3,32|0|EADEAC,0|26|4F9371,8|26|325A47,16|26|47956C,24|26|57B688,32|26|63CD98",
+        0,
+        0.9,
+        "商店内部城墙"
     )
 }
