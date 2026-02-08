ddcf69968e99c46d571864a029ba13def74217ce
Bob8259 <azikaban_yuki@outlook.com>
2026-02-08 17:45:32 +0800

    1

--- app/src/main/java/com/coc/zkqcode/jar/code/colorschema/MyColors.kt
+++ app/src/main/java/com/coc/zkqcode/jar/code/colorschema/MyColors.kt
@@ -20,46 +20,20 @@ import com.coc.zkqcode.jar.code.colorschema.colorpackage.NightBaseTutorial
 import com.coc.zkqcode.jar.code.colorschema.colorpackage.UIColors
 import com.coc.zkqcode.jar.code.colorschema.colorpackage.WorkerColors
 
-object MyColors : IWorkerColors by WorkerColors, IUIColors by UIColors, IFeatureColors by FeatureColors,
-    IMainBaseTutorial by MainBaseTutorial, INightBaseTutorial by NightBaseTutorial,
-    IMainBaseTraining by MainBaseTraining, INightBaseResourcesColors by NightBaseResourcesColors,
-    INightBaseObstaclesRemovalColors by NightBaseObstaclesRemovalColors,
+object MyColors : IWorkerColors by WorkerColors, IUIColors by UIColors, IFeatureColors by FeatureColors, IMainBaseTutorial by MainBaseTutorial, INightBaseTutorial by NightBaseTutorial,
+    IMainBaseTraining by MainBaseTraining, INightBaseResourcesColors by NightBaseResourcesColors, INightBaseObstaclesRemovalColors by NightBaseObstaclesRemovalColors,
     INightBaseTrainingColors by NightBaseTrainingColors {
     //以下是前期检测颜色
     val UpgradeToTH6: ColorSchema = ColorSchema.parse(
-        700,
-        189,
-        725,
-        221,
-        "1919FF",
-        "2|-6|1919FF,2|-6|1919FF,6|0|1919FF,8|2|1919FF,11|4|1919FF,7|8|1919FF,0|7|1919FF,0|5|1919FF,1|1|1919FF",
-        0,
-        0.9,
-        "需要将大本营升至6级"
+        700, 189, 725, 221, "1919FF", "2|-6|1919FF,2|-6|1919FF,6|0|1919FF,8|2|1919FF,11|4|1919FF,7|8|1919FF,0|7|1919FF,0|5|1919FF,1|1|1919FF", 0, 0.9, "需要将大本营升至6级"
     )
 
     //The followings are Night Base Upgrade color
     val InnerShopArrow: ColorSchema = ColorSchema.parse(
-        736,
-        313,
-        781,
-        363,
-        "00A3FD",
-        "9|0|00A2FD,18|0|01A0FF,27|0|08A7FF,36|0|13AEFF,0|25|02ABFF,9|25|01ABFE,18|25|01A6FD,27|25|04A1FF,36|25|0C9BFF",
-        0,
-        0.9,
-        "商店内部箭头"
+        736, 313, 781, 363, "00A3FD", "9|0|00A2FD,18|0|01A0FF,27|0|08A7FF,36|0|13AEFF,0|25|02ABFF,9|25|01ABFE,18|25|01A6FD,27|25|04A1FF,36|25|0C9BFF", 0, 0.9, "商店内部箭头"
     )
     val WallInShop: ColorSchema = ColorSchema.parse(
-        618,
-        401,
-        658,
-        453,
-        "EADEAC",
-        "8|0|567188,16|0|5C96CD,24|0|7BDBF3,32|0|EADEAC,0|26|4F9371,8|26|325A47,16|26|47956C,24|26|57B688,32|26|63CD98",
-        0,
-        0.9,
-        "商店内部城墙"
+        618, 401, 658, 453, "EADEAC", "8|0|567188,16|0|5C96CD,24|0|7BDBF3,32|0|EADEAC,0|26|4F9371,8|26|325A47,16|26|47956C,24|26|57B688,32|26|63CD98", 0, 0.9, "商店内部城墙"
     )
 
 }
