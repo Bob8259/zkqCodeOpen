2f746db65d6e230c8bc2810cc5c4548a289ef3b4
Bob8259 <azikaban_yuki@outlook.com>
2026-02-09 23:12:27 +0800

    making upgrade. need to make tap builder list

--- app/src/main/java/com/coc/zkqcode/jar/code/nightbase/upgradehelper/UpgradeExistingBuildings.kt
+++ app/src/main/java/com/coc/zkqcode/jar/code/nightbase/upgradehelper/UpgradeExistingBuildings.kt
@@ -1,8 +1,11 @@
 package com.coc.zkqcode.jar.code.nightbase.upgradehelper
 
 import com.coc.zkqcode.core.util.basic.ShowMessage
+import com.coc.zkqcode.core.util.basic.delayWithMultiplier
 import com.coc.zkqcode.core.util.fileactions.LogHelper.logAndStop
+import com.coc.zkqcode.core.util.touchactions.TouchActions
 import com.coc.zkqcode.jar.code.nightbase.NightBaseUpgradeBuildings.checkContinueBuild
+import com.coc.zkqcode.jar.code.universal.clickRightBottom
 import com.coc.zkqcode.jar.code.universal.enterMainScreen
 import com.coc.zkqcode.jar.code.universal.smalltools.getConfigRuntime
 import com.coc.zkqcode.jar.ui.schema.details.NightBaseBuildingsPriority
@@ -10,17 +13,33 @@ import com.coc.zkqcode.jar.ui.schema.details.NightBaseBuildingsPriority
 object UpgradeExistingBuildings {
     suspend fun upgradeAllExistingBuildings(buildings: List<String>): Boolean {
         val orderedList = getOrderedList(buildings)
-        // Implementation for upgrading buildings using orderedList will go here
-        for (building in orderedList){
-            if(checkContinueBuild()){
-                
+        for (building in orderedList) {
+            clickRightBottom()
+            if (checkContinueBuild()) {
+                findSpecificBuilding(building)
             }
+            clickRightBottom()
         }
         return enterMainScreen()
     }
-    private fun findSpecificBuilding(){
 
+    private suspend fun findSpecificBuilding(buildingName: String): Boolean {
+        var found = false
+        ShowMessage("准备寻找$buildingName")
+        iterateNightBaseBuildingUpgradeList { result ->
+            val building = result.buildings.find { it.name == buildingName }
+            if (building != null) {
+                TouchActions.tap(building.x, building.y)
+                delayWithMultiplier(1500)
+                found = true
+                true
+            } else {
+                false
+            }
+        }
+        return found
     }
+
     private fun getOrderedList(buildings: List<String>): List<String> {
         val priorityMap = NightBaseBuildingsPriority.all.associate { settingDef ->
             val priorityStr = getConfigRuntime(settingDef.key)
@@ -28,8 +47,8 @@ object UpgradeExistingBuildings {
             settingDef.displayName to priority
         }
 
-        return buildings.sortedBy { 
-            priorityMap[it] ?: logAndStop("Building $it not found in upgrade priority list. Please check NightBaseBuildingsPriority configuration.") 
+        return buildings.sortedBy {
+            priorityMap[it] ?: logAndStop("Building $it not found in upgrade priority list. Please check NightBaseBuildingsPriority configuration.")
         }
     }
 }
