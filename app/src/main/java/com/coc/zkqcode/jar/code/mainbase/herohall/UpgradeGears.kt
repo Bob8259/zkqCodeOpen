5608dc99ca22fe235a763a69db0609f774e4286c
Bob8259 <azikaban_yuki@outlook.com>
2026-04-04 20:48:59 +0800

    Add upgradeGears function to handle gear upgrades in HeroHall
    This commit introduces a new file, UpgradeGears.kt, containing the upgradeGears function, which manages the gear upgrade process based on configuration settings. It includes a message display for the upgrade preparation and a placeholder for the actual upgrade logic, enhancing the gameplay experience by streamlining gear management.

---
+++ app/src/main/java/com/coc/zkqcode/jar/code/mainbase/herohall/UpgradeGears.kt
@@ -0,0 +1,8 @@
+package com.coc.zkqcode.jar.code.mainbase.herohall
+
+import com.coc.zkqcode.jar.code.universal.smalltools.getBooleanConfigRuntime
+import com.coc.zkqcode.jar.ui.schema.Schema
+
+suspend fun upgradeGear(): Boolean{
+    if (!getBooleanConfigRuntime(Schema.MAIN_BASE_SETTINGS.))
+}
